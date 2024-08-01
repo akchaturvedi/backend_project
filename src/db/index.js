fce0d6684bf891341613220b73db2e23f330beed
Adesh kumar chaturvedi <adeshchaturvedi51@gmail.com>
2024-08-01 08:27:14 +0530

    connect to mongo altas

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try{
+
+        const connectInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`mongodb connected !!  DB HOST : ${connectInstance.connection.host}`  );
+
+    }
+
+    catch (error)
+    {
+        console.log("mongodb connection error", error);
+        process.exit(1);
+
+    }
+}
+
+export default connectDB;
